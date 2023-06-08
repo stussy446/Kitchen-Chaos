8fde2d9cba607396228b327ee715bc1228de4a14
Steve Rector <steverector27@gmail.com>
2023-06-08 11:44:56 -0700

    Add ability to know where kitchen object is/who its parent is

--- Assets/Scripts/KitchenObject.cs
+++ Assets/Scripts/KitchenObject.cs
@@ -5,4 +5,36 @@ using UnityEngine;
 public class KitchenObject : MonoBehaviour
 {
     [SerializeField] private KitchenObjectSO kitchenObjectSO;
+
+    private ClearCounter clearCounter;
+
+    public KitchenObjectSO KitchenObjectSO
+    {
+        get { return kitchenObjectSO; }
+    }
+
+    public void SetClearCounter(ClearCounter clearCounter)
+    {
+        if (this.clearCounter != null)
+        {
+            this.clearCounter.ClearKitchenObject();
+        }
+
+        this.clearCounter = clearCounter;
+
+        if(clearCounter.HasKitchenObject())
+        {
+            Debug.LogError("Counter already has a kitchen object");
+        }
+
+        clearCounter.SetKitchenObject(this);
+
+        transform.parent = clearCounter.GetKitchenObjectFollowTransform();
+        transform.localPosition = Vector3.zero;
+    }
+
+    public ClearCounter GetClearCounter()
+    {
+        return clearCounter;
+    }
 }
